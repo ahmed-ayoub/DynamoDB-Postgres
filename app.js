5f2093817f4dc2d51423c95787f0687ef52ec4d2
ahmed-ayoub <ahmed.bebo.ayoub@gmail.com>
2016-04-26 22:37:08 +0200

    show me urself plz
    Hi Ayoubkooo

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello AYOOOOOOOOOOOOOOOOOOOOOOOOOOB World\n");
 });
-server.listen(8080);
+server.listen(3000);
 console.log("Server running at http://127.0.0.1:8080/")
