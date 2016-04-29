6dd2b3d508fa125a2ae2c5cdad84dc0108ea6671
root <root@ip-172-30-1-237.ec2.internal>
2016-04-29 23:27:39 +0000

    commit4

--- api.js
+++ api.js
@@ -9,5 +9,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js")(app);
 
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+    console.log("start Listening on port %s...", server.address().port);
 });
