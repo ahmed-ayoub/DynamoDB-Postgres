1afbbeda7f36eef9ad328fecc6142c011f178c2c
ahmed-ayoub <ahmed.bebo.ayoub@gmail.com>
2016-04-30 01:06:14 +0200

    commit1

--- api.js
+++ api.js
@@ -1,14 +1,13 @@
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-// Hi Ayoub
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Hi Ahmed
-console.log('hiiiiiiiii');
+
 var routes = require("./routes/routes.js")(app);
- 
+
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
