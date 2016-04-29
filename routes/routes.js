3d26c79af678c971ccbc35048ac23c8f41852b2a
ahmed-ayoub <ahmed.bebo.ayoub@gmail.com>
2016-04-30 01:11:21 +0200

    commit3

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,93 @@
 var appRouter = function(app) {
- 
+
 app.get("/", function(req, res) {
-    res.send("Hello World");
+
+
+        var pg = require('pg');
+        var connectionString = 'pg://postgres:ayoub@localhost:5432/myfirstdb';
+
+        var client = new pg.Client(connectionString);
+        client.connect();
+
+        //var query = client.query("insert into emp(_id,_fname,_lname,_age) values (2,\'Mohamed\',\'Ayoub\', 38)");
+        //query.on('end', function() { client.end(); });
+        //console.log("hi ayoub");
+    //res.send("Hello World");
+
+        ///////////////////////////////////////////////////// Start My Code
+        var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM emp;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+         // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+        ////////////////////////////////////////////////////// end my code
+
+        });
+
+app.post("/emp", function(req, res) {
+
+        var pg = require('pg');
+        var connectionString = 'pg://postgres:ayoub@localhost:5432/myfirstdb';
+
+        var client = new pg.Client(connectionString);
+        client.connect();
+
+        var results = [];
+ // Grab data from http request
+    var data = {id: req.body.id, fname: req.body.fname, lname: req.body.lname,age: req.body.age};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO emp(_id,_fname,_lname,_age) values($1, $2, $3, $4)", [data.id, data.fname,data.lname, data.age]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM emp");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+
+    });
 });
- 
 }
- 
+
 module.exports = appRouter;
+
+
